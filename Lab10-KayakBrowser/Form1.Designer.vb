d4c9f80a514670b55b29482d8414d98fbc6f2e6b
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-04-25 11:38:32 -0400

    Finished Assignment

--- Lab10-KayakBrowser/Form1.Designer.vb
+++ Lab10-KayakBrowser/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,32 +20,91 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.KayaksDataSet = New Lab10_KayakBrowser.KayaksDataSet()
+        Me.KayakTypesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.KayakTypesTableAdapter = New Lab10_KayakBrowser.KayaksDataSetTableAdapters.KayakTypesTableAdapter()
+        Me.TableAdapterManager = New Lab10_KayakBrowser.KayaksDataSetTableAdapters.TableAdapterManager()
+        Me.NameListBox = New System.Windows.Forms.ListBox()
+        Me.DescriptionLabel1 = New System.Windows.Forms.Label()
+        CType(Me.KayaksDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KayakTypesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(37, 46)
+        Me.Label1.Location = New System.Drawing.Point(12, 21)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(201, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Select a kayak type to view a description"
         '
+        'KayaksDataSet
+        '
+        Me.KayaksDataSet.DataSetName = "KayaksDataSet"
+        Me.KayaksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'KayakTypesBindingSource
+        '
+        Me.KayakTypesBindingSource.DataMember = "KayakTypes"
+        Me.KayakTypesBindingSource.DataSource = Me.KayaksDataSet
+        '
+        'KayakTypesTableAdapter
+        '
+        Me.KayakTypesTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.KayakTypesTableAdapter = Me.KayakTypesTableAdapter
+        Me.TableAdapterManager.UpdateOrder = Lab10_KayakBrowser.KayaksDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'NameListBox
+        '
+        Me.NameListBox.DataBindings.Add(New System.Windows.Forms.Binding("SelectedValue", Me.KayakTypesBindingSource, "Name", True))
+        Me.NameListBox.DataSource = Me.KayakTypesBindingSource
+        Me.NameListBox.DisplayMember = "Name"
+        Me.NameListBox.FormattingEnabled = True
+        Me.NameListBox.Location = New System.Drawing.Point(15, 47)
+        Me.NameListBox.Name = "NameListBox"
+        Me.NameListBox.Size = New System.Drawing.Size(211, 121)
+        Me.NameListBox.TabIndex = 3
+        '
+        'DescriptionLabel1
+        '
+        Me.DescriptionLabel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DescriptionLabel1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.KayakTypesBindingSource, "Description", True))
+        Me.DescriptionLabel1.Location = New System.Drawing.Point(251, 47)
+        Me.DescriptionLabel1.Name = "DescriptionLabel1"
+        Me.DescriptionLabel1.Size = New System.Drawing.Size(285, 121)
+        Me.DescriptionLabel1.TabIndex = 4
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(507, 261)
+        Me.ClientSize = New System.Drawing.Size(548, 189)
+        Me.Controls.Add(Me.DescriptionLabel1)
+        Me.Controls.Add(Me.NameListBox)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.KayaksDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.KayakTypesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents KayaksDataSet As KayaksDataSet
+    Friend WithEvents KayakTypesBindingSource As BindingSource
+    Friend WithEvents KayakTypesTableAdapter As KayaksDataSetTableAdapters.KayakTypesTableAdapter
+    Friend WithEvents TableAdapterManager As KayaksDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents NameListBox As ListBox
+    Friend WithEvents DescriptionLabel1 As Label
 End Class
--- Lab10-KayakBrowser/Form1.vb
+++ Lab10-KayakBrowser/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Private Sub KayakTypesBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
+        Me.Validate()
+        Me.KayakTypesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.KayaksDataSet)
 
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'KayaksDataSet.KayakTypes' table. You can move, or remove it, as needed.
+        Me.KayakTypesTableAdapter.Fill(Me.KayaksDataSet.KayakTypes)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
 End Class
